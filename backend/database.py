e7f97123556dfda1bbcd1a7c8198bb9bbc28f18d
hetbhagatji09 <hetbhagatji09@gmail.com>
2025-09-06 19:31:04 +0530

    configured database on docker

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:@localhost:5432/job_resume_match"
+DATABASE_URL = "postgresql://bunny0901:0901@localhost:5434/job-resume-match?sslmode=disable"
+
 
 engine = create_engine(DATABASE_URL,echo=True   )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
